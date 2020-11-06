3504a070aa3bd70eafc88ca96cfa669c790e5db8
Cristian <cvg97@misena.edu.co>
2020-11-06 15:26:58 -0500

    Improved Stack

--- libraries/libsystem/utils/Stack.h
+++ libraries/libsystem/utils/Stack.h
@@ -1,33 +1,32 @@
-template <typename T>
-struct Stack {
+#pragma once
+
+template <class T>
+class Stack {
+    
     T value;
-    struct Stack *next;
+    Stack *next;
+    
+    public:
+        bool isEmpty(Stack *top){
+            return top == nullptr;
+        }
+        
+        void push(Stack **top, T value){
+            Stack *node = new Stack();
+            node->next = *top;
+            *top = node;
+            node->value = value;
+        }
+        
+        T peek(Stack *top){
+             return !isEmpty(top) ? top->value : 0;
+        }
+        
+        Stack *pop(Stack **top){
+            Stack *temp =  *top;
+            *top = temp->next;
+            temp->next = nullptr;
+            delete temp;
+            return *top;
+        }
 };
-
-typedef struct Stack<char> stack;
-
-template <typename T>
-void push(stack **top, T value){
-   
-   stack *node = new stack();
-   node->next = *top;
-   *top = node;
-   node->value = value;
-}
-
-stack* pop(stack **top){
-    stack *temp =  *top;
-    *top = temp->next;
-    temp->next = nullptr;
-    delete temp;
-    return *top;
-}
-
-template <typename T>
-T peek (stack *top){
-    return top->value;
-}
-
-bool isEmpty(stack *top){
-    return top == nullptr;
-}
